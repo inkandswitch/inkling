359cec5e35fef47a8ea2e627b44fe144fba16b9f
Ivan Reese <ivanreese@gmail.com>
2023-08-12 21:29:28 -0600

    Add some little helpers I whipped up during today's debugging exploration

---
+++ InkbaseV2/src/lib/helpers.ts
@@ -0,0 +1,12 @@
+// A debug view of an object's properties. Clearing is useful when debugging a single object at 60hz.
+export function debugTable(obj: Object, clear = true) {
+  if (clear) console.clear()
+  console.table(objectWithSortedKeys(obj))
+}
+
+// My kingdom for a standard library that includes a key-sorted Map.
+export function objectWithSortedKeys(obj) {
+  return Object.keys(obj)
+    .sort()
+    .reduce((acc, k) => ({ ...acc, [k]: obj[k] }), {})
+}
