7c7f6b1de10399a6bd67b122272909c733e388f1
Marcel Goethals <marcel@wolkenmachine.nl>
2023-08-17 16:12:48 +0200

    Cleanup freehand stroke

--- Prototypes/MultistrokeAnalysis/src/app/strokes/FreehandStroke.ts
+++ Prototypes/MultistrokeAnalysis/src/app/strokes/FreehandStroke.ts
@@ -6,16 +6,6 @@ import generateId from "../generateId";
 import ControlPoint from "./ControlPoint";
 import { Position, PositionWithPressure } from "../../lib/types";
 
-import Polygon from "../../lib/polygon";
-
-// import hull from "hull.js"
-// import Voronoi from "voronoi"
-
-
-
-
-// var voronoi = new Voronoi();
-
 export const strokeSvgProperties = {
   stroke: "rgba(0, 0, 0, .5)",
   // fill: 'rgba(0, 0, 0, .5)',
@@ -103,110 +93,6 @@ export default class FreehandStroke {
     return Vec.angle(Vec.sub(this.controlPoints[1].position, this.controlPoints[0].position));
   }
 
-  generateOutline2(){
-    this.points = rdp_simplify(this.points, 2)
-    let shape = new Shape([this.points], true, true, true, true)
-    
-    shape = shape.offset( 10, {
-      jointType: 'jtRound',
-      endType: 'etOpenRound',
-      miterLimit: 2.0,
-      roundPrecision: 0.25
-    })
-    console.log(shape);
-    this.outline = shape.paths.map(path=>{
-      let newpath = path.map(pt=>{
-        return {x: pt.X, y: pt.Y}
-      })
-      newpath.push({
-        x: shape.paths[0][0].X,
-        y: shape.paths[0][0].Y
-      })
-      return newpath
-    })
-    
-
-    console.log(this.outline);
-    
-    
-
-    //const clipperPath = new Clipper.Path();
-
-    // this.points = rdp_simplify(this.points, 1)
-    // let offset = offsetPath(this.points, 20)
-    // this.outline = offset
-    // console.log(offset);
-    
-    //let path = new paper.Path()
-    // this.points.forEach(point=>{
-    //   path.add(new paper.Point(point?.x, point?.y))
-    // })
-
-    // PaperOffset.offsetStroke(path, 10, { cap: 'round' })
-    // console.log(path);
-  }
-
-
-  generateOutline(){
-    const OFFSET = 10;
-    const CIRCLESTEP = 16;
-    let circle: any[] = [];
-    for (let i = 0; i < CIRCLESTEP; i++) {
-      circle.push([
-        Math.sin((i / CIRCLESTEP) * Math.PI*2) * (OFFSET),
-        Math.cos((i / CIRCLESTEP) * Math.PI*2) * (OFFSET)
-      ]);
-    }
-
-    let lastPt: PositionWithPressure | null = null;
-    let hullPoints = this.points.flatMap(pt=>{
-      if(pt === null) return [];
-      if(lastPt != null && Vec.dist(pt, lastPt) < 10) {
-        return [];  
-      }
-      lastPt = pt;
-      return circle.map(c=>{
-        return [
-          c[0] + pt.x,
-          c[1] + pt.y
-        ]
-      });
-    })
-
-    this.outline = hull(hullPoints, 10).map(pt=>{
-      return Vec(pt[0], pt[1]);
-    })
-
-    let smoothedOutline = gaussianSmooth(this.outline, 5);
-
-    // // simplify
-    // let new_outline = [this.outline[0]];
-    // let lastPoint = this.outline[0];
-    // this.outline.forEach(pt=>{
-    //   if(Vec.dist(lastPoint, pt) > 10) {
-    //     new_outline.push(pt);
-    //     lastPoint = pt;
-    //   }
-    // })
-    // this.outline = new_outline;
-    
-    const bbox = {xl: 0, xr: window.innerWidth, yt: 0, yb: window.innerHeight};
-    console.log(bbox);
-    
-    let diagram = voronoi.compute(smoothedOutline, bbox);
-    // filter edges
-    let skeleton: any[] = [];
-    diagram.edges.forEach(edge=>{
-      if(Polygon.isPointInside(this.outline, edge.va) && Polygon.isPointInside(this.outline, edge.vb)) {
-        skeleton.push({a: edge.va, b: edge.vb});
-      }
-    })
-
-    this.diagram = skeleton;
-
-    console.log(this.diagram);
-  }
-
   onControlPointMove() {
     this.dirty = true;
   }
@@ -226,17 +112,6 @@ export default class FreehandStroke {
     });
     const path = generatePathFromPoints(this.points);
     updateSvgElement(this.element, { d: path, stroke: this.selected ? "rgba(255, 0, 0, 1)": "rgba(0, 0, 0, .5)" });
-
-    // outline path
-    // const outlinePath = this.outline.flatMap(path=>generatePathFromPoints(path));
-    // updateSvgElement(this.outlineElement, {d: outlinePath});
-
-    // skeleton path
-    // let skeletonPath = "";
-    // this.diagram.forEach(edge=>{      
-    //     skeletonPath += `M ${edge.a.x} ${edge.a.y} L ${edge.b.x} ${edge.b.y}`
-    // })
-    // updateSvgElement(this.skeletonElement, {d: skeletonPath});
   }
 
   render() {
@@ -268,77 +143,3 @@ function farthestPair(points: Position[]): [Position, Position] {
   }
   return [mdp1!, mdp2!];
 }
-
-// Smooth
-function gaussianSmooth(polygon, sigma = 1) {
-  const kernelSize = Math.ceil(3 * sigma); // Determine kernel size based on sigma
-  const kernel = generateGaussianKernel(kernelSize, sigma);
-  const halfKernelSize = Math.floor(kernelSize/2);
-  
-  const smoothedPolygon: any[] = [];
-  
-  for (let i = 0; i < polygon.length; i++) {
-    let smoothedPoint = { x: 0, y: 0 };
-    
-    for (let j = -halfKernelSize; j <= halfKernelSize; j++) {
-      const index = (i + j + polygon.length) % polygon.length;
-      const weight = kernel[j + halfKernelSize];
-      
-      smoothedPoint.x += weight * polygon[index].x;
-      smoothedPoint.y += weight * polygon[index].y;
-    }
-    
-    smoothedPolygon.push(smoothedPoint);
-  }
-  
-  return smoothedPolygon;
-}
-
-function generateGaussianKernel(size, sigma) {
-  const kernel: any[] = [];
-  const sigmaSquared = sigma * sigma;
-  const constant = 1 / (2 * Math.PI * sigmaSquared);
-  let sum = 0;
-  
-  for (let i = 0; i < size; i++) {
-    const distance = i - Math.floor(size / 2);
-    const weight = constant * Math.exp(-(distance * distance) / (2 * sigmaSquared));
-    kernel.push(weight);
-    sum += weight;
-  }
-  
-  // Normalize the kernel
-  for (let i = 0; i < size; i++) {
-    kernel[i] /= sum;
-  }
-  
-  return kernel;
-}
-
-function offsetPath(points, offsetDistance) {
-  if (points.length < 2 || typeof offsetDistance !== 'number') {
-    throw new Error('Invalid input');
-  }
-
-  const offsetPath: any[] = [];
-
-  for (let i = 0; i < points.length; i++) {
-    const point = points[i];
-    const prevPoint = points[i - 1] || point;
-    const nextPoint = points[i + 1] || point;
-
-    const angle = Math.atan2(nextPoint.y - prevPoint.y, nextPoint.x - prevPoint.x) + Math.PI / 2;
-    const offsetX = Math.cos(angle) * offsetDistance;
-    const offsetY = Math.sin(angle) * offsetDistance;
-
-    const offsetPoint = {
-      x: point.x + offsetX,
-      y: point.y + offsetY
-    };
-
-    offsetPath.push(offsetPoint);
-  }
-
-  return offsetPath;
-}
-
