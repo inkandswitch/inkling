6eff4432b7e112cb89b323269d7a964105e5ba90
Alessandro Warth <alexwarth@gmail.com>
2023-08-22 14:55:54 -0700

    clean up FreehandSelection

--- InkbaseV2/src/app/FreehandSelection.ts
+++ InkbaseV2/src/app/FreehandSelection.ts
@@ -1,119 +1,120 @@
-import { Position } from "../lib/types";
-import Vec from "../lib/vec";
-import Events, {Event} from "./NativeEvents";
-import Page from "./Page"
-import FreehandStroke from "./strokes/FreehandStroke"
-import StrokeGroup from "./strokes/StrokeGroup"
+import Vec from '../lib/vec';
+import Events, { Event } from './NativeEvents';
+import Page from './Page';
+import FreehandStroke from './strokes/FreehandStroke';
+import StrokeGroup from './strokes/StrokeGroup';
 
 export default class FreehandSelection {
-    page: Page
-    selectedStrokes = new Set<FreehandStroke>();
-    clusterSelectionIndex = 0;
+  readonly selectedStrokes = new Set<FreehandStroke>();
+  clusterSelectionIndex = 0;
 
-    // Interaction State
-    fingerDown: (Event | null) = null;
-    fingerMoved: (Event | null) = null;
+  // Interaction State
+  fingerDown: Event | null = null;
+  fingerMoved: Event | null = null;
 
-    // Current Group
-    strokeGroup: (StrokeGroup | null) = null;
+  // Current Group
+  strokeGroup: StrokeGroup | null = null;
 
+  constructor(private readonly page: Page) {}
 
-    constructor(page: Page) {
-      this.page = page;
-    }
+  update(events: Events) {
+    const fingerDown = events.did('finger', 'began');
+    if (fingerDown) {
+      this.fingerDown = fingerDown;
+      const found = this.page.findFreehandStrokeNear(fingerDown.position);
 
-    update(events: Events) {
-      const fingerDown = events.did('finger', 'began');
-      if (fingerDown !== undefined) {
-        this.fingerDown = fingerDown;
-        let found = this.page.findFreehandStrokeNear(fingerDown.position);
-        
-        // Register longpress
-        window.setTimeout((_:any)=>{
-          if(this.fingerDown !== null && (this.fingerMoved === null || Vec.dist(this.fingerDown.position, this.fingerMoved.position) < 10)) { 
-            console.log("Longhold");
+      // Register longpress
+      window.setTimeout(() => {
+        if (
+          this.fingerDown &&
+          (!this.fingerMoved ||
+            Vec.dist(this.fingerDown.position, this.fingerMoved.position) < 10)
+        ) {
+          console.log('Longhold');
 
-            if(!this.strokeGroup && this.selectedStrokes.size > 0) {
-              let group = new StrokeGroup(this.page.svg, this.selectedStrokes);
-              this.page.strokeGroups.push(group);
-              this.strokeGroup = group;
-            }
-            
+          if (!this.strokeGroup && this.selectedStrokes.size > 0) {
+            const group = new StrokeGroup(this.page.svg, this.selectedStrokes);
+            this.page.strokeGroups.push(group);
+            this.strokeGroup = group;
           }
-        }, 750);
-
-        if(found) {
-          this.fingerDownOnStroke(found)
-        } else {
-          this.fingerDownOnEmptySpace()
         }
-      }
+      }, 750);
 
-      if(this.fingerDown !== null) {
-        const fingerMoved = events.did('finger', 'moved', this.fingerDown.id);
-        if(fingerMoved !== undefined) {
-          this.fingerMoved = fingerMoved
-        }
-
-        const fingerEnded = events.did('finger', 'ended', this.fingerDown.id);
-        if(fingerEnded !== undefined) {
-          this.fingerDown = null
-          this.fingerMoved = null
-        }
-      }
-      
-    }
-
-    fingerDownOnStroke(stroke: FreehandStroke){
-      if(this.selectedStrokes.has(stroke)) {
-        let clusters = this.page.clusters.getClustersForStroke(stroke);
-        
-        if(clusters== undefined) return;
-
-        this.clusterSelectionIndex++;
-        let cluster = clusters.get(this.clusterSelectionIndex % (clusters.size()));
-
-        this.clearSelection();
-        for(const stroke of cluster) {
-          this.select(stroke);
-        }
+      if (found) {
+        this.fingerDownOnStroke(found);
       } else {
-        this.select(stroke);
-        if(this.selectedStrokes.size > 1) {
-          for(const stroke of this.selectedStrokes) {
-            this.page.clusters.addClusterForStroke(stroke, this.selectedStrokes);
-          }
-        }
-
-        this.clusterSelectionIndex = 0;
+        this.fingerDownOnEmptySpace();
       }
     }
 
-    fingerDownOnEmptySpace(){
-        this.clearSelection();
-    }
+    if (this.fingerDown) {
+      const fingerMoved = events.did('finger', 'moved', this.fingerDown.id);
+      if (fingerMoved) {
+        this.fingerMoved = fingerMoved;
+      }
 
-    render(){
-      // if(this.selectedStrokes) {
-      //   this.selectedStrokes.render(svg)
-      // }
+      const fingerEnded = events.did('finger', 'ended', this.fingerDown.id);
+      if (fingerEnded) {
+        this.fingerDown = null;
+        this.fingerMoved = null;
+      }
     }
+  }
 
-    select(stroke: FreehandStroke){
-        stroke.select();
-        this.selectedStrokes.add(stroke);
-    }
+  fingerDownOnStroke(stroke: FreehandStroke) {
+    if (this.selectedStrokes.has(stroke)) {
+      const clusters = this.page.clusters.getClustersForStroke(stroke);
 
-    clearSelection(){
-      if(this.selectedStrokes === null) {
+      if (!clusters) {
         return;
       }
-      
-      for(const stroke of this.selectedStrokes) {
-        stroke.deselect();
+
+      this.clusterSelectionIndex++;
+      const cluster = clusters.get(
+        this.clusterSelectionIndex % clusters.size()
+      );
+
+      this.clearSelection();
+      for (const stroke of cluster) {
+        this.select(stroke);
+      }
+    } else {
+      this.select(stroke);
+      if (this.selectedStrokes.size > 1) {
+        for (const stroke of this.selectedStrokes) {
+          this.page.clusters.addClusterForStroke(stroke, this.selectedStrokes);
+        }
       }
 
-      this.selectedStrokes = new Set();
-      this.strokeGroup = null;
+      this.clusterSelectionIndex = 0;
     }
+  }
+
+  fingerDownOnEmptySpace() {
+    this.clearSelection();
+  }
+
+  render() {
+    // if (this.selectedStrokes) {
+    //   this.selectedStrokes.render(svg)
+    // }
+  }
+
+  select(stroke: FreehandStroke) {
+    stroke.select();
+    this.selectedStrokes.add(stroke);
+  }
+
+  clearSelection() {
+    if (!this.selectedStrokes) {
+      return;
+    }
+
+    for (const stroke of this.selectedStrokes) {
+      stroke.deselect();
+    }
+
+    this.selectedStrokes.clear();
+    this.strokeGroup = null;
+  }
 }
