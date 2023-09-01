443c3d2292b5a915ed0eea1663eea2eaf60cae12
Alessandro Warth <alexwarth@gmail.com>
2023-09-01 12:04:26 -0700

    some changes in preparation for constraints owning variables

--- InkbaseV2/src/app/constraints.ts
+++ InkbaseV2/src/app/constraints.ts
@@ -114,6 +114,7 @@ interface ClusterForSolver {
   handles: Handle[];
   handleGetsXFrom: Map<Handle, Variable>;
   handleGetsYFrom: Map<Handle, Variable>;
+  // constrainedState: StateSet;
 }
 
 let _clustersForSolver: Set<ClusterForSolver> | null = null;
@@ -148,14 +149,20 @@ function getClustersForSolver(): Set<ClusterForSolver> {
 
   _clustersForSolver = new Set(
     Array.from(clusters).map(cluster => {
-      const { constraints, variables, handleGetsXFrom, handleGetsYFrom } =
-        getDedupedConstraintsAndVariables(cluster.constraints);
+      const {
+        constraints,
+        variables,
+        handleGetsXFrom,
+        handleGetsYFrom,
+        // constrainedState,
+      } = getDedupedConstraintsAndVariables(cluster.constraints);
       return {
         constraints,
         variables,
         handles: getHandlesIn(constraints),
         handleGetsXFrom,
         handleGetsYFrom,
+        // constrainedState,
       };
     })
   );
@@ -609,7 +616,9 @@ function addConstraint<C extends Constraint>(
   createNew: (keyGenerator: ConstraintKeyGenerator) => C,
   onClash: (olderConstraint: C) => Variable[]
 ): { constraint: C; variables: Variable[] } {
-  let constraint = findConstraint<C>(keyGenerator.key);
+  let constraint = allConstraints.find(
+    constraint => constraint.key === keyGenerator.key
+  ) as C | undefined;
   let variables: Variable[];
   if (constraint) {
     variables = onClash(constraint);
@@ -618,12 +627,6 @@ function addConstraint<C extends Constraint>(
     variables = constraint.variables;
   }
   return { constraint, variables };
-}
-
-function findConstraint<C extends Constraint>(key: string) {
-  return allConstraints.find(constraint => constraint.key === key) as
-    | C
-    | undefined;
 }
 
 // #endregion adding constraints
@@ -941,17 +944,13 @@ export function vertical(a: Handle, b: Handle) {
   return variableEquals(ax, bx);
 }
 
-export function length(
-  a: Handle,
-  b: Handle,
-  length = new Variable(Vec.dist(a.position, b.position))
-) {
+export function length(a: Handle, b: Handle) {
   return addConstraint(
     new ConstraintKeyGenerator('length', [[a, b]], []),
     keyGenerator => {
-      return new Length(a, b, length, keyGenerator);
+      return new Length(a, b, keyGenerator);
     },
-    olderConstraint => olderConstraint.onClash(length)
+    olderConstraint => olderConstraint.onClash()
   );
 }
 
@@ -959,35 +958,38 @@ class Length extends Constraint {
   constructor(
     public readonly a: Handle,
     public readonly b: Handle,
-    public readonly length: Variable,
     keyGenerator: ConstraintKeyGenerator
   ) {
-    super([a, b], [length], keyGenerator);
+    super(
+      [a, b],
+      [new Variable(Vec.dist(a.position, b.position))],
+      keyGenerator
+    );
+  }
+
+  get length() {
+    return this.variables[0];
   }
 
   getError([aPos, bPos]: Position[], [length]: number[]): number {
     return Vec.dist(aPos, bPos) - length;
   }
 
-  onClash(newerConstraintOrLength: this | Variable): Variable[] {
-    const thatLength =
-      newerConstraintOrLength instanceof Length
-        ? newerConstraintOrLength.length
-        : newerConstraintOrLength;
-    variableEquals(this.length, thatLength);
-    return [thatLength];
+  onClash(newerConstraint?: this): Variable[] {
+    if (newerConstraint) {
+      variableEquals(this.length, newerConstraint.length);
+      return [newerConstraint.length];
+    } else {
+      return this.variables;
+    }
   }
 }
 
-export function angle(
-  a1: Handle,
-  a2: Handle,
-  angle = new Variable(Vec.angle(Vec.sub(a2.position, a1.position)))
-) {
+export function angle(a1: Handle, a2: Handle) {
   return addConstraint(
     new ConstraintKeyGenerator('angle', [[a1, a2]], []),
-    keyGenerator => new Angle(a1, a2, angle, keyGenerator),
-    olderConstraint => olderConstraint.onClash(a1, a2, angle)
+    keyGenerator => new Angle(a1, a2, keyGenerator),
+    olderConstraint => olderConstraint.onClash(a1, a2)
   );
 }
 
@@ -995,10 +997,17 @@ class Angle extends Constraint {
   constructor(
     public readonly a1: Handle,
     public readonly a2: Handle,
-    public readonly angle: Variable,
     keyGenerator: ConstraintKeyGenerator
   ) {
-    super([a1, a2], [angle], keyGenerator);
+    super(
+      [a1, a2],
+      [new Variable(Vec.angle(Vec.sub(a2.position, a1.position)))],
+      keyGenerator
+    );
+  }
+
+  get angle() {
+    return this.variables[0];
   }
 
   getError(
@@ -1052,28 +1061,34 @@ class Angle extends Constraint {
   }
 
   onClash(newerConstraint: this): Variable[];
-  onClash(a1: Handle, a2: Handle, angle: Variable): Variable[];
-  onClash(newerConstraintOrA1: this | Handle, a2?: Handle, angle?: Variable) {
-    let a1: Handle;
-    let thatAngle: Variable;
+  onClash(a1: Handle, a2: Handle): Variable[];
+  onClash(newerConstraintOrA1: this | Handle, a2?: Handle) {
+    let a1: Handle, angle: Variable | undefined;
     if (newerConstraintOrA1 instanceof Angle) {
-      ({ a1, a2, angle: thatAngle } = newerConstraintOrA1);
+      ({ a1, a2, angle } = newerConstraintOrA1);
     } else {
       a1 = newerConstraintOrA1;
-      thatAngle = angle!;
+      a2 = a2!; // help the type checker understand that this is not undefined
     }
 
     if (this.a1 === a1 && this.a2 === a2) {
       // exactly the same thing!
-      variableEquals(this.angle, thatAngle);
+      if (angle) {
+        variableEquals(this.angle, angle);
+      } else {
+        angle = this.angle;
+      }
     } else {
       // same points but different order
-      const diff = new Variable(this.angle.value - thatAngle.value);
+      if (!angle) {
+        angle = new Variable(Vec.angle(Vec.sub(a2.position, a1.position)));
+      }
+      const diff = new Variable(this.angle.value - angle.value);
       fixedValue(diff);
-      variablePlus(this.angle, thatAngle, diff);
+      variablePlus(this.angle, angle, diff);
     }
 
-    return [thatAngle];
+    return [angle];
   }
 }
 
