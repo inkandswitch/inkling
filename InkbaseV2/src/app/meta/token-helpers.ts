6e13a8379f2d665c01e0695940309ea5fd4821b9
Alessandro Warth <alexwarth@gmail.com>
2023-10-06 15:47:03 -0700

    improve and expand token helpers

--- InkbaseV2/src/app/meta/token-helpers.ts
+++ InkbaseV2/src/app/meta/token-helpers.ts
@@ -1,8 +1,15 @@
 import LabelToken from './LabelToken';
 import NumberToken from './NumberToken';
+import PropertyPicker from './PropertyPicker';
 import Token from './Token';
 
 export type TokenWithVariable = NumberToken | LabelToken;
 
-export const isTokenWithVariable = (token: Token): token is TokenWithVariable =>
+export const isTokenWithVariable = (
+  token: Token | null
+): token is TokenWithVariable =>
   token instanceof NumberToken || token instanceof LabelToken;
+
+export const isPropertyPicker = (
+  token: Token | null
+): token is PropertyPicker => token instanceof PropertyPicker;
