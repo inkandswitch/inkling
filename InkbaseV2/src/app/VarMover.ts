5c5e9abee86b46703915ac3cd12ddcba0910752d
Alessandro Warth <alexwarth@gmail.com>
2023-10-19 00:05:16 -0700

    fix bug in VarMover

--- InkbaseV2/src/app/VarMover.ts
+++ InkbaseV2/src/app/VarMover.ts
@@ -2,12 +2,13 @@ import { Variable } from './constraints';
 
 interface Move {
   variable: Variable;
-  wasLocked: boolean;
+  unlockWhenDone: boolean;
   initialValue: number;
   finalValue: number;
   durationSeconds: number;
   easeFn: (t: number) => number;
   initialTime: number;
+  done: boolean;
 }
 
 const moves: Move[] = [];
@@ -20,40 +21,44 @@ function move(
 ) {
   moves.push({
     variable,
-    wasLocked: variable.isLocked,
+    unlockWhenDone: !variable.isLocked,
     initialValue: variable.value,
     finalValue,
     durationSeconds,
     easeFn,
     initialTime: 0,
+    done: false,
   });
 }
 
 function update(dt: number, t: number) {
   for (const move of moves) {
+    if (move.done) {
+      moves.splice(moves.indexOf(move, 1));
+      if (move.unlockWhenDone) {
+        move.variable.unlock();
+      }
+      continue;
+    }
+
     if (move.initialTime === 0) {
       move.initialTime = t;
     }
 
     const {
       variable,
-      wasLocked,
       initialValue,
       finalValue,
       durationSeconds,
       easeFn,
       initialTime,
     } = move;
+
     const pct = Math.min((t - initialTime) / durationSeconds, 1);
     variable.lock(initialValue + (finalValue - initialValue) * easeFn(pct));
-    // console.log('moved to', variable.value);
 
     if (pct === 1) {
-      // done with this move
-      moves.splice(moves.indexOf(move, 1));
-      if (!wasLocked) {
-        variable.unlock();
-      }
+      move.done = true;
     }
   }
 }
