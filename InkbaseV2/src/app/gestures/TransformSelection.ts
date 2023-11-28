9ccc820ce3c51d60299390a961f505bb7cf4afca
Alessandro Warth <alexwarth@gmail.com>
2023-11-28 11:19:53 -0800

    make the linter happy

--- InkbaseV2/src/app/gestures/TransformSelection.ts
+++ InkbaseV2/src/app/gestures/TransformSelection.ts
@@ -8,17 +8,70 @@ import Handle from '../ink/Handle';
 import * as constraints from '../constraints';
 
 export function transformSelection(ctx: EventContext): Gesture | void {
-  if (Selected.size > 0 && ctx.pseudo) {
-    let firstFinger: FingerState | null = null;
-    let secondFinger: FingerState | null = null;
-    const handlePositions = new Map<Handle, Position>();
+  if (!(Selected.size > 0 && ctx.pseudo)) {
+    return;
+  }
 
-    const makeMoveTransform = (p: Position) =>
-      TransformationMatrix.identity().translate(p.x, p.y);
-    const makePinchTransform = (a: Position, b: Position) =>
-      TransformationMatrix.fromLine(Vec.avg(a, b), b);
+  let firstFinger: FingerState | null = null;
+  let secondFinger: FingerState | null = null;
+  const handlePositions = new Map<Handle, Position>();
 
-    function resetTransform() {
+  const makeMoveTransform = (p: Position) =>
+    TransformationMatrix.identity().translate(p.x, p.y);
+  const makePinchTransform = (a: Position, b: Position) =>
+    TransformationMatrix.fromLine(Vec.avg(a, b), b);
+
+  function resetTransform() {
+    let transform: TransformationMatrix;
+    if (firstFinger && secondFinger) {
+      transform = makePinchTransform(
+        firstFinger.position,
+        secondFinger.position
+      );
+    } else if (firstFinger) {
+      transform = makeMoveTransform(firstFinger.position);
+    } else {
+      return;
+    }
+    transform = transform.inverse();
+    handlePositions.clear();
+    Selected.forEach(obj => {
+      if (obj instanceof Handle) {
+        handlePositions.set(obj, transform.transformPoint(obj.position));
+      }
+    });
+  }
+
+  return new Gesture('Transform Selection', {
+    claim: 'fingers',
+
+    began(ctx) {
+      if (!firstFinger) {
+        firstFinger = ctx.state as FingerState;
+        resetTransform();
+      } else if (!secondFinger) {
+        secondFinger = ctx.state as FingerState;
+        resetTransform();
+      }
+    },
+
+    ended(ctx) {
+      if (ctx.event.id === firstFinger?.id) {
+        firstFinger = secondFinger;
+        secondFinger = null;
+        resetTransform();
+      } else if (ctx.event.id === secondFinger?.id) {
+        secondFinger = null;
+        resetTransform();
+      }
+      Selected.forEach(obj => {
+        if (obj instanceof Handle) {
+          constraints.finger(obj).remove();
+        }
+      });
+    },
+
+    moved(ctx) {
       let transform: TransformationMatrix;
       if (firstFinger && secondFinger) {
         transform = makePinchTransform(
@@ -30,67 +83,16 @@ export function transformSelection(ctx: EventContext): Gesture | void {
       } else {
         return;
       }
-      transform = transform.inverse();
-      handlePositions.clear();
+
       Selected.forEach(obj => {
         if (obj instanceof Handle) {
-          handlePositions.set(obj, transform.transformPoint(obj.position));
+          const oldPos = handlePositions.get(obj);
+          if (oldPos) {
+            obj.position = transform.transformPoint(oldPos);
+            constraints.finger(obj);
+          }
         }
       });
-    }
-
-    return new Gesture('Transform Selection', {
-      claim: 'fingers',
-
-      began(ctx) {
-        if (!firstFinger) {
-          firstFinger = ctx.state as FingerState;
-          resetTransform();
-        } else if (!secondFinger) {
-          secondFinger = ctx.state as FingerState;
-          resetTransform();
-        }
-      },
-
-      ended(ctx) {
-        if (ctx.event.id == firstFinger?.id) {
-          firstFinger = secondFinger;
-          secondFinger = null;
-          resetTransform();
-        } else if (ctx.event.id == secondFinger?.id) {
-          secondFinger = null;
-          resetTransform();
-        }
-        Selected.forEach(obj => {
-          if (obj instanceof Handle) {
-            constraints.finger(obj).remove();
-          }
-        });
-      },
-
-      moved(ctx) {
-        let transform: TransformationMatrix;
-        if (firstFinger && secondFinger) {
-          transform = makePinchTransform(
-            firstFinger.position,
-            secondFinger.position
-          );
-        } else if (firstFinger) {
-          transform = makeMoveTransform(firstFinger.position);
-        } else {
-          return;
-        }
-
-        Selected.forEach(obj => {
-          if (obj instanceof Handle) {
-            const oldPos = handlePositions.get(obj);
-            if (oldPos) {
-              obj.position = transform.transformPoint(oldPos);
-              constraints.finger(obj);
-            }
-          }
-        });
-      },
-    });
-  }
+    },
+  });
 }
